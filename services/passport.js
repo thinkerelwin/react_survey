3b23dc119f4dec34445a36ff6e90c34223d62887
Chi-Heng Huang <polozero1@yahoo.com.tw>
2018-12-11 22:21:39 +0800

    initial commit

---
+++ services/passport.js
@@ -0,0 +1,40 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
+const keys = require('../config/keys')
+
+const User = mongoose.model('users')
+
+passport.serializeUser((user, done) => {
+  // user.id refer to id that are generated from mongoDB in mLab
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user)
+    })
+})
+
+passport.use
+  (new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken, refreshToken, profile, done) => {
+    // console.log(accessToken)
+    User.findOne({ googleId: profile.id })
+      .then((existingUser) => {
+        if (existingUser) {
+          done(null, existingUser)
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => {
+              done(null, user)
+            })
+        }
+      })
+  })
+)
