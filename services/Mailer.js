60e9442ae754db1e670597250a1d5c6e801432b9
thinkerelwin <polozero1@yahoo.com.tw>
2018-12-24 14:57:09 +0800

    at section 10, lecture 132

---
+++ services/Mailer.js
@@ -0,0 +1,29 @@
+const sendgrid = require('sendgrid')
+const helper = sendgrid.mail
+const keys = require('../config/keys')
+
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super()
+
+    this.from_email = new helper.Email('no-reply@emaily.com')
+    this.subject = subject
+    this.body = new helper.Conetent('text/html', content)
+    this.recipients = this.formatAddresses(recipients)
+
+    this.addContent(this.body) // came from built in function in helper.Mail
+    this.addClickTracking()
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email}) => {
+      return new helper.Email(email)
+    })
+  }
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings()
+    const clickTracking = new helper.ClickTracking(true, true)
+  }
+}
+
+module.exports = Mailer
